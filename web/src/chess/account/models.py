c51850bab49016f24684156241f9a88cdbf11c39
Vladimir <pappardelle@mail.ru>
2020-11-22 21:52:58 +0300

    Added login and logout functions

--- web/src/chess/account/models.py
+++ web/src/chess/account/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserStats:
+    user = models.OneToOneField(User, on_delete=models.SET_NULL)
+    wins_count = models.IntegerField(default=0)
+    losses_count = models.IntegerField(default=0)
--- web/src/chess/account/views.py
+++ web/src/chess/account/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.contrib.auth import logout, authenticate, login
 
 
-def test(request):
+def test(request: HttpRequest) -> HttpResponse:
     return render(request, 'account/test.html')
+
+
+def login_page(request: HttpRequest):
+    user = authenticate(request, username='username', password='password')
+    if user is not None:
+        login(request, user)
+
+
+def logout_func(request: HttpRequest):
+    logout(request)
