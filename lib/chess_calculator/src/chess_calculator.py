3c4a331ac74049db9b196ad05b4d964e75d69da7
Yaroslav <bondarev.yaroslav@outlook.com>
2020-11-22 15:34:22 +0300

    add base methods defenitions; update .gitignore; update requieremnts.txt for both projects;

---
+++ lib/chess_calculator/src/chess_calculator.py
@@ -0,0 +1,14 @@
+import stockfish
+
+def get_next_move(fen: str) -> str:
+    """
+    Return next move based on fen string
+    """
+    pass
+
+
+def is_best_move(move: str) -> bool:
+    """
+    Return boolean value based on move passed as argument
+    """
+    pass
