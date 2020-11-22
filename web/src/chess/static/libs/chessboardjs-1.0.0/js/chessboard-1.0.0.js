ceb962643c5e89d4479b7778e363f0304ec3ead4
Yaroslav <bondarev.yaroslav@outlook.com>
2020-11-22 18:23:36 +0300

    add board component; fix chessboard static

--- web/src/chess/static/libs/chessboardjs-1.0.0/js/chessboard-1.0.0.js
+++ web/src/chess/static/libs/chessboardjs-1.0.0/js/chessboard-1.0.0.js
@@ -576,7 +576,7 @@
     // default piece theme is wikipedia
     if (!config.hasOwnProperty('pieceTheme') ||
         (!isString(config.pieceTheme) && !isFunction(config.pieceTheme))) {
-      config.pieceTheme = 'img/chesspieces/wikipedia/{piece}.png'
+      config.pieceTheme = '/static/libs/chessboardjs-1.0.0/img/chesspieces/wikipedia/{piece}.png'
     }
 
     // animation speeds
