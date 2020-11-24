38babf373795b9803ae796476f8da0ed35c5b1d7
Creator148 <bak-home@mail.ru>
2020-11-24 18:14:26 +0300

    add:get_next_move,is_best_move

--- lib/chess_calculator/src/chess_calculator.py
+++ lib/chess_calculator/src/chess_calculator.py
@@ -1,14 +1,13 @@
 import stockfish
 
+from stockfish import Stockfish
+
+stockfish = Stockfish("/Users/bak-h/stockfish/stockfish64")
+#chessEngine = Engine('./stockfish_64', param={'Threads': 5, 'Ponder': None})
+
 def get_next_move(fen: str) -> str:
-    """
-    Return next move based on fen string
-    """
-    pass
+    return stockfish.get_best_move(fen)
 
 
 def is_best_move(move: str) -> bool:
-    """
-    Return boolean value based on move passed as argument
-    """
-    pass
+        return stockfish.is_move_correct(str)
