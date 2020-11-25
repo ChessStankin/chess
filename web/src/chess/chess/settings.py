cef3ebade72ed504db199b6e69630d50127e2b01
Yaroslav <bondarev.yaroslav@outlook.com>
2020-11-25 19:43:41 +0300

    update settings.py

--- web/src/chess/chess/settings.py
+++ web/src/chess/chess/settings.py
@@ -31,7 +31,9 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1',
+                 '0.0.0.0',
+                 '20.54.75.41']
 
 # Application definition
 
