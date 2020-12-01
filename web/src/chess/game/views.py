cbddda58dd59ee0ab31ac260eb14e1edeea0e93d
Vladimir <pappardelle@mail.ru>
2020-12-01 13:35:49 +0300

    Added basic layout. Fixed some functions.

--- web/src/chess/game/views.py
+++ web/src/chess/game/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 # Create your views here.
 
+
 def test(request: HttpRequest) -> HttpResponse:
     return render(request, 'game/test.html')
 
